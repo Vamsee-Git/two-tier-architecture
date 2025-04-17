0781f734bc31654e89d6845598ed2512a0c00487
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-17 11:24:58 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  required_version = "~> 1.11.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
 module "vpc" {
   source = "./modules/vpc"
 
