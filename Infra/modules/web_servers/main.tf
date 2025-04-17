08d2ca91777f0f8ca54c1da2955ed06b65efcf6a
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-17 11:50:00 +0530

    Update main.tf

--- Infra/modules/web_servers/main.tf
+++ Infra/modules/web_servers/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "web" {
   ami                  = var.ami_id
   instance_type        = var.instance_type
   subnet_id            = element(var.subnets, count.index)
+  user_data           = element(var.user_data, count.index)
   vpc_security_group_ids = [var.security_group]
 
   tags = {
