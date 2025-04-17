7298cfff557cefa7dc1cb806096076e9113ffc83
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-17 10:48:25 +0530

    Create variables.tf

---
+++ Infra/modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "azs" {
+  description = "Availability zones"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "Public subnets"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "Private subnets"
+  type        = list(string)
+}
