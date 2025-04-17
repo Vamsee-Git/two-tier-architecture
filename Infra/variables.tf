1af1ec9cf23de9632593764369ee7ea6918b2d02
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-17 11:15:35 +0530

    Update variables.tf

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -1,25 +1,65 @@
-variable "instance_count" {
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "Public subnets"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "Private subnets"
+  type        = list(string)
+}
+
+variable "sg_name_prefix" {
+  description = "Name prefix for security groups"
+  type        = string
+}
+
+variable "web_name_prefix" {
+  description = "Name prefix for web servers"
+  type        = string
+}
+
+variable "web_instance_count" {
   description = "Number of web server instances"
   type        = number
   default     = 2
 }
 
-variable "ami_id" {
+variable "web_ami_id" {
   description = "AMI ID for web servers"
   type        = string
 }
 
-variable "instance_type" {
+variable "web_instance_type" {
   description = "Instance type for web servers"
   type        = string
 }
 
-variable "instance_class" {
+variable "db_name_prefix" {
+  description = "Name prefix for database"
+  type        = string
+}
+
+variable "db_instance_class" {
   description = "Instance class for database"
   type        = string
 }
 
-variable "allocated_storage" {
+variable "db_allocated_storage" {
   description = "Allocated storage for database"
   type        = number
 }
@@ -36,20 +76,5 @@ variable "db_username" {
 
 variable "db_password" {
   description = "Database password"
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
-
-variable "subnets" {
-  description = "Subnets"
-  type        = list(string)
-}
-
-variable "security_group" {
-  description = "Security group ID"
   type        = string
 }
