dc5a818537547657d0dc7f78c3d74f7e63491fcd
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-17 11:35:01 +0530

    Create backend.tf

---
+++ Infra/backend.tf
@@ -0,0 +1,9 @@
+terraform{
+  backend "s3" {
+    bucket         = "terraform-state-backend-vamsee"
+    key            = "terraform/two-tier"
+    region         = "us-west-1"
+    encrypt        = true
+    dynamodb_table = "Dynamodb-lock"
+  }
+}
