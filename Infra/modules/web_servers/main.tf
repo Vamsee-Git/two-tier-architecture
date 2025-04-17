01788735fb9835fae03809bced9de1063b473d1c
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-17 10:53:30 +0530

    Update main.tf

--- Infra/modules/web_servers/main.tf
+++ Infra/modules/web_servers/main.tf
@@ -1,31 +1,9 @@
-resource "aws_security_group" "web_sg" {
-  vpc_id = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.name}-web-sg"
-  }
-}
-
 resource "aws_instance" "web" {
-  count         = var.instance_count
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = element(var.subnets, count.index)
-  security_groups = [aws_security_group.web_sg.name]
+  count                = var.instance_count
+  ami                  = var.ami_id
+  instance_type        = var.instance_type
+  subnet_id            = element(var.subnets, count.index)
+  vpc_security_group_ids = [var.security_group]
 
   tags = {
     Name = "${var.name}-web-${count.index}"
